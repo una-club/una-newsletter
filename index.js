db17619a710618572f47bd9172c1b1c156cdd061
Mickael Tardy <mickael.a.tardy@gmail.com>
2017-10-31 17:18:44 +0100

    Cleanup index.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-var base64Img = require('base64-img');
-
-var marked = require('marked');
-
-var Mustache = require('mustache');
-
+const base64Img = require('base64-img');
+const marked = require('marked');
+const Mustache = require('mustache');
 const fs = require('fs')
 
-content = fs.readFileSync('src/template.html')
+template = fs.readFileSync('src/template.html')
 api_key_env   = process.env.MAILGUN_TOKEN;
 
 var api_key = api_key_env
@@ -18,7 +15,7 @@ var data = {
   from: 'UNA Communication <support@una-club.fr>',
   to: 'system@mail.una-club.fr',
   subject: '[UNA] Newsletter du 1 Novembre',
-  html: content.toString().replace(/(?:\r\n|\r|\n)/g, '')
+  html: template.toString().replace(/(?:\r\n|\r|\n)/g, '')
 };
 
 mailgun.messages().send(data, function (error, body) {
